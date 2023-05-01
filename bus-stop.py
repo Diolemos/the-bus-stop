90226ab712c74bc0f66de65a02d42dca24d6b2ab
Dio Lemos <pedro.devsc@outlook.com>
2023-05-01 03:05:37 -0300

    add header

---
+++ bus-stop.py
@@ -0,0 +1,24 @@
+print('''
+         
+ ______  __  __   ______       ______   __  __   ______       ______   ______  ______   ______  
+/\__  _\/\ \_\ \ /\  ___\     /\  == \ /\ \/\ \ /\  ___\     /\  ___\ /\__  _\/\  __ \ /\  == \ 
+\/_/\ \/\ \  __ \\ \  __\     \ \  __< \ \ \_\ \\ \___  \    \ \___  \\/_/\ \/\ \ \/\ \\ \  _-/ 
+   \ \_\ \ \_\ \_\\ \_____\    \ \_____\\ \_____\\/\_____\    \/\_____\  \ \_\ \ \_____\\ \_\   
+    \/_/  \/_/\/_/ \/_____/     \/_____/ \/_____/ \/_____/     \/_____/   \/_/  \/_____/ \/_/   
+                                                                                                
+
+    
+                                                           ''')
+_continue = input("Welcome, press enter to continue" )
+
+print("You are at the bus stop, late at night. when a **FROG** breaks your routine.")
+print("It is big and different. You are bored. You feel this is a special nignt.\n even so you're kind of afraid")
+print("Kiss it? (type: 'yes' or 'no')")
+kiss_frog = input().lower()
+print('''
+     o-o
+    /,_,\
+  ,Mm/_\mM, ''')
+
+if kiss_frog == 'yes':
+    print('It was poisonous.\nGame over.')
